a4b6797dedcfc051fd11e9c8d97da31fc6c5f5cd
Mateus De Jesus Santos <mateussant0sc0osta@outlook.com>
2025-02-09 10:21:40 -0300

    Tabularization of values ​​and organization of the code
    The code was restructured, removing some lines of comments that were not useful, and the code was tabulated using index-oriented Pandas.

--- CompanyRegistration.py
+++ CompanyRegistration.py
@@ -10,44 +10,42 @@ except FileNotFoundError:
     print("File not found. Creating a new one")
     object_json = {}  # Cria um dicionário vazio se o arquivo não existir
 
-# Criando CPF aleatorios
+
 adc = input('To add?\n1-Yes\n2-No\n>')
 if adc == '1':
-    import random
+
     # Editar os dados
     NameCompany = input("Enter company name: ")
     CnpjCompany = int(input("Enter the CNPJ: "))
+    Category = "Limpeza"
     
+    # Cria o codigo de 5 digitos
+    import random
     code5digts = ''
-    for i in range(4):
+    for i in range(5):
         code5digts += str(random.randint(0, 5))
         
-    # Adicionar as informações ao dicionário
+    # Adiciona as informações ao dicionário
     if code5digts not in object_json:
         object_json.update(
             {   
-                'Code' : code5digts,
-                "record":
+                f"RegistrationCode-{code5digts}":
                 {
-                    'Name' : NameCompany,
+                    'NAME' : NameCompany,
                     'CNPJ' : CnpjCompany,
+                    'CATEGORY': Category,
                 }
             }
         )
 else:
-    print("")
-
-# Mostrando empresas cadastradas:
-"""for codes in object_json.keys():
-    print("Name:",object_json[codes][0]["Name"],"\nCNPJ:",object_json[codes][0]["CNPJ"])"""
-
-print(pd.json_normalize(object_json))
-#pd.read_json(object_json, orient = 'values')
-#delete = object_json.pop("0334")
-#print(object_json)
+    # Mostra empresas cadastradas:
+    json_index = json.dumps(object_json)
+    print(pd.read_json(json_index, orient = 'index'))
 
 # Salvar os dados de volta no arquivo
 with open('./company.data.json', 'w') as arquivo_json:
     json.dump(object_json, arquivo_json, indent=4)
 
 print("File updated successfully!")
+
+"""CRIAR CATEGORIAS"""
